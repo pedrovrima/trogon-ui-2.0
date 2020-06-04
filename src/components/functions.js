5db64e897f7480b021da4c95c0e44e1fee68538f
pedrovrima <pedrovrima@gmail.com>
2020-06-04 13:12:46 -0300

    moves checker from iput_field to functions

--- src/components/functions.js
+++ src/components/functions.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useSelector, useDispatch } from "react-redux";
+import validateDate from "validate-date";
 
 export function NavigationButtons(handleSub = () => null) {
   const effort_stage = useSelector((state) => state.data_stage);
@@ -67,8 +68,9 @@ export function NameCreator(title = "", unit = null) {
 }
 
 export function createCapture(protocol_variables) {
-  
-  let cap_vars=protocol_variables.map((variable)=> {return({name:variable.name,a_value:""})})
+  let cap_vars = protocol_variables.map((variable) => {
+    return { name: variable.name, a_value: "" };
+  });
   return {
     capture_time: "",
     net_number: "",
@@ -86,4 +88,119 @@ export function createCapture(protocol_variables) {
 
 export function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
+}
+
+export function createChecker(
+  unit,
+  type,
+  value,
+  user_options,
+  new_options,
+  options
+) {
+  const checkCont = () => {
+    switch (unit) {
+      case "percentage":
+        return {
+          check: () =>value === "NA" ?  false :
+            !Number(value) ? !Number(value) : value > 100 || value < 0,
+          message: "Valores devem estar entre 0 e 100",
+          props: {
+            options: {
+              uppercase: true,
+            },
+          },
+        };
+
+      case "nets":
+        return {
+          check: () => value > user_options,
+          message:
+            "Esta estação possui apenas " + user_options + " registradas",
+          props: {
+            options: {
+              uppercase: true,
+            },
+          },
+        };
+
+      default:
+        return {
+          check: () => value === "NA" ?  false : !Number(value),
+          message: "Deve ser um número",
+          props: {
+            options: {
+              uppercase: true,
+            },
+          },
+        };
+    }
+  };
+
+  switch (type) {
+    case "val":
+      return {
+        check: () =>
+          value === "NA" ? false : new_options[0].indexOf(value) < 0,
+        message: "Valores permitidos:" + new_options,
+        props: {
+          options: {
+            uppercase: true,
+          },
+        },
+      };
+
+    case "date":
+      return {
+        check: () => !validateDate(value, "boolean", "dd/mm/yyyy"),
+        message: "Data inválida",
+        props: {
+          options: { date: true },
+        },
+      };
+
+    case "cont":
+      let pre_checker = checkCont();
+      return pre_checker;
+
+    case "time":
+      return {
+        check: () => value === "",
+        message: "No message",
+        props: {
+          options: {
+            time: true,
+            timePattern: ["h", "m"],
+          },
+        },
+      };
+
+    case "cap_time":
+      return {
+        check: () => value.length > 3,
+        message: "Máximo de 3 caracteres",
+      };
+
+    case "spp_name":
+      return {
+        check: (value) => {
+          return value
+            ? options.map((spp) => spp.code).indexOf(value.target.value) < 0
+            : null;
+        },
+        message: "Espécies inválida",
+        props: {
+          options: {
+            uppercase: true,
+          },
+        },
+      };
+
+    case "band_number":
+      return {
+        check: () => null,
+      };
+    default:
+      break;
+  }
 }
--- src/components/input_field/index.js
+++ src/components/input_field/index.js
@@ -4,9 +4,8 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { Typeahead } from "react-bootstrap-typeahead";
 import { useDispatch } from "react-redux";
-import validateDate from "validate-date";
 import Cleave from "cleave.js/react";
-import { NameCreator } from "../functions";
+import {createChecker, NameCreator } from "../functions";
 
 export default function RegularField({
   variable = null,
@@ -21,16 +20,15 @@ export default function RegularField({
   title = null,
   value = null,
   param = null,
-protocol_options = null,
+  protocol_options = null,
 
   ...props
 }) {
   let new_options = [];
-  
-  if(protocol_options){
-    new_options.push(protocol_options.map((opt) => opt.value_oama));
 
-  } else if(user_options) {
+  if (protocol_options) {
+    new_options.push(protocol_options.map((opt) => opt.value_oama));
+  } else if (user_options) {
     new_options.push(user_options);
   }
 
@@ -45,8 +43,6 @@ protocol_options = null,
   function setInvalidation(data, key) {
     return { type: "FORM_VALIDATION", data: data, key: key };
   }
-console.log(value)
-
 
   const createTitle = (title, unit) => {
     let newT = NameCreator(title, unit);
@@ -57,105 +53,9 @@ console.log(value)
     );
   };
 
-  const checkCont = () => {
-    switch (unit) {
-      case "percentage":
-        return {
-          check: () => value > 100 && value > 0,
-          message: "Valores devem estar entre 0 e 100",
-          props: {},
-        };
 
-      case "nets":
-        return {
-          check: () => value > user_options,
-          message:
-            "Esta estação possui apenas " + user_options + " registradas",
-          props: {},
-        };
 
-      default:
-        return {
-          check: () => value === "",
-          message: "Valores devem estar entre 0 e 100",
-          props: {},
-        }
-      };
-  };
-
-  const createChecker = () => {
-    switch (type) {
-      case "val":
-        return {
-          check: () => new_options[0].indexOf(value) < 0,
-          message: "Valores permitidos:" + new_options,
-          props: {
-            options:{
-              uppercase: true
-            }
-          },
-        };
-
-      case "date":
-        return {
-          check: () => validateDate(value),
-          message: "Data inválida",
-          props: {
-            options: { date: true },
-          },
-        };
-
-      case "cont":
-        let pre_checker = checkCont();
-        return pre_checker;
-
-      case "time":
-        return {
-          check: () => value === "",
-          message: "No message",
-          props: {
-            options: {
-              time: true,
-              timePattern: ["h", "m"],
-            },
-          },
-        };
-
-      case "cap_time":
-        return {
-          check: () => value.length > 3,
-          message: "Máximo de 3 caracteres",
-        };
-
-      case "spp_name":
-        return {
-          check: (value) => {
-            return value
-              ? options.map((spp) => spp.code).indexOf(value.target.value) < 0
-              : null;
-          },
-          message: "Espécies inválida",
-          props:{
-            options:{
-              uppercase: true
-            }
-          }
-        };
-
-      case "band_number":
-        return {
-          check: () => null,
-          props: {
-              options:{prefix:"F"}
-          }
-        };
-        default:
-        break
-    }
-        
-  };
-
-  const checker = createChecker();
+  const checker = createChecker (unit,type,value, user_options, new_options,options)
   const checkInvalid = (value) => {
     let is_invalid = checker.check(value);
     setInvalid(is_invalid);
@@ -166,9 +66,8 @@ console.log(value)
     <Form.Group as={Col}>
       {createTitle(title, unit)}
       <Row>
-        {["spp_name","band_number"].indexOf(type) < 0? (
+        {["spp_name", "band_number"].indexOf(type) < 0 ? (
           <Form.Control
-            
             as={Cleave}
             name={name}
             value={value}
@@ -187,7 +86,7 @@ console.log(value)
             onFocus={() => setInvalid(0)}
             {...checker.props}
             {...props}
-            options={options?options:""}
+            options={options ? options : ""}
           />
         )}
 
