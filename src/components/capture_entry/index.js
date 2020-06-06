977ba0f26b39ddac79326255c023b27eb9b00a0a
pedrovrima <pedrovrima@gmail.com>
2020-06-06 17:52:30 -0300

    add captureEnd strage

--- src/components/capture_entry/index.js
+++ src/components/capture_entry/index.js
@@ -1,27 +1,20 @@
-import React from 'react'
-import {  useSelector } from 'react-redux'
-import CaptureInit from "../capture_init"
-import CaptureVals from "../capture_vals"
-
+import React from "react";
+import { useSelector } from "react-redux";
+import CaptureInit from "../capture_init";
+import CaptureVals from "../capture_vals";
+import CaptureEnd from "../capture_end";
 
 export default function CaptureEntry(capture_index) {
-let capture_stage = useSelector((state)=>state.capture_stage)
-    return (
-
-        <>
-            
-            {
-                capture_stage===0?
-                <CaptureInit ></CaptureInit>:
-                capture_stage===1?
-                <CaptureVals></CaptureVals> :
-                null
-            }
-            
-        </>
-
-        )
-
-
-
+  let capture_stage = useSelector((state) => state.capture_stage);
+  return (
+    <>
+      {capture_stage === 0 ? (
+        <CaptureInit></CaptureInit>
+      ) : capture_stage === 1 ? (
+        <CaptureVals></CaptureVals>
+      ) : capture_stage === 2 ? (
+        <CaptureEnd></CaptureEnd>
+      ) : null}
+    </>
+  );
 }
