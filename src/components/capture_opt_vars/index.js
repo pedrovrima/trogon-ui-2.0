be045678bffd704e1485e99757320740bd72a118
pedrovrima <pedrovrima@gmail.com>
2020-06-07 18:05:29 -0300

    creates modal for extra vars

---
+++ src/components/capture_opt_vars/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {Modal} from "react-bootstrap"
+
+
+export default function VarModal(props) {
+  
+    return (
+    
+<Modal show={props.modalState} onHide={()=>props.setModal(false)}>    
+<Modal.Header closeButton >
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        {props.captureVars.map((capVar)=>{
+            return(
+                <button className="btn btn-primary m-2" onClick={()=>props.addVariable(capVar.name)}>
+                    {capVar.portuguese_label}
+                </button>
+            )
+
+        })}
+
+
+        </Modal.Body>
+    
+
+    </Modal>
+  );
+}
