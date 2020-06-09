8a457e4d29e420485ee0d310b0507eb56af086b1
pedrovrima <pedrovrima@gmail.com>
2020-06-09 15:31:20 -0300

    add local storage process here

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, {  useEffect } from 'react';
+import React, {  useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import DataEntry from './components/data_entry';
@@ -8,6 +8,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faMinusSquare, faPlusSquare, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 import NavBar from './components/navbar';
+import PreEnter from "./components/entry_loader"
+
 
 library.add(faMinusSquare, faPlusSquare, faPlus, faMinus)
 
@@ -15,9 +17,18 @@ library.add(faMinusSquare, faPlusSquare, faPlus, faMinus)
 function App() {
 
   useEffect(() => {
-    setLocalStorage()
+    setLocalStorage().then((data)=>{
+      console.log(data)
+      if(data){
+      store.dispatch({ type: 'LOCAL_SETTER', data: {...data} })}
+    }).then(store.dispatch({ type: 'LOADER', data: 1 }))
+    
   }, [])
 
+  
+
+  
+  let [stage,setStage]=useState("inital")
   return (
     <>
       <Provider store={store}>
@@ -25,7 +36,9 @@ function App() {
             <NavBar />
             <div className="container-fluid container-all">
           <div className="row align-items-center ml-4 mr-4 h-100 justify-content-center">
+            {stage==="inital"?< PreEnter setStage={setStage}></PreEnter>:
             <DataEntry />
+}
           </div>
         </div>
       </Provider>
